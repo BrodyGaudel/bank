c7830441d57b6f81510c6fa992eea07c00ea9e33
Brody Gaudel <57298219+BrodyGaudel@users.noreply.github.com>
2023-08-13 12:40:30 +0100

    Eureka Discovery Service

--- discovery-service/src/main/java/com/mounanga/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/mounanga/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package com.mounanga.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
