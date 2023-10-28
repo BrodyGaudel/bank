c8c812323a7607c397d1dd94e5061e5c392680e7
Brody Gaudel <57298219+BrodyGaudel@users.noreply.github.com>
2023-10-28 23:01:17 +0100

    add spring security

---
+++ account-service/src/main/java/com/brodygaudel/accountservice/securities/SecParams.java
@@ -0,0 +1,19 @@
+package com.brodygaudel.accountservice.securities;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class SecParams {
+    @Value("${secret}")
+    private String secret;
+
+    @Value("${expired-time}")
+    private Long expiredTime;
+
+    public SecParams(){
+        super();
+    }
+}
