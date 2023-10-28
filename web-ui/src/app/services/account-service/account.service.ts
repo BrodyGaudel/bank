665984b28a3ed80737681edf3228dd8fbbccaa4b
Brody Gaudel <57298219+BrodyGaudel@users.noreply.github.com>
2023-10-28 23:02:10 +0100

    add web-ui security

--- web-ui/src/app/services/account-service/account.service.ts
+++ web-ui/src/app/services/account-service/account.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {AccountModel} from "../../models/account.model";
+import {AuthService} from "../auth-service/auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +11,36 @@ export class AccountService {
 
   private host: string = 'http://localhost:8888/ACCOUNT-SERVICE/bank/v2/accounts';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService){}
+
+  private getHeader() :HttpHeaders {
+    let jwt: string = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    return new HttpHeaders({"Authorization": jwt});
+  }
 
   public getById(id: string) :Observable<AccountModel>{
-    return this.http.get<AccountModel>(this.host + '/get/' + id);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.get<AccountModel>(this.host + '/get/' + id, {headers:httpHeaders});
   }
 
   public getByCustomerId(customerId: string) :Observable<AccountModel>{
-    return this.http.get<AccountModel>(this.host + '/find/' + customerId);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.get<AccountModel>(this.host + '/find/' + customerId, {headers:httpHeaders});
   }
 
   public save(model: AccountModel) :Observable<AccountModel>{
-    return this.http.post<AccountModel>(this.host + '/save', model);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.post<AccountModel>(this.host + '/save', model, {headers:httpHeaders});
   }
 
   public updateStatus(model: AccountModel) :Observable<AccountModel>{
-    return this.http.put<AccountModel>(this.host + '/update', model);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.put<AccountModel>(this.host + '/update', model, {headers:httpHeaders});
   }
 
   public deleteById(id: string) :Observable<Object>{
-    return this.http.delete(this.host + '/delete/' + id);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.delete(this.host + '/delete/' + id, {headers:httpHeaders});
   }
 }
--- web-ui/src/app/services/operation-service/operation.service.ts
+++ web-ui/src/app/services/operation-service/operation.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {CreditModel} from "../../models/credit.model";
 import {Observable} from "rxjs";
 import {DebitModel} from "../../models/debit.model";
 import {OperationModel} from "../../models/operation.model";
 import {HistoryModel} from "../../models/history.model";
+import {AuthService} from "../auth-service/auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,31 @@ export class OperationService {
 
   private host: string = 'http://localhost:8888/ACCOUNT-SERVICE/bank/v2/operations';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService){}
+
+  private getHeader() :HttpHeaders {
+    let jwt: string = this.authService.getToken();
+    jwt = "Bearer "+jwt;
+    return new HttpHeaders({"Authorization": jwt});
+  }
 
   public credit(model: CreditModel) :Observable<CreditModel>{
-    return this.http.post<CreditModel>(this.host + '/credit', model);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.post<CreditModel>(this.host + '/credit', model, {headers:httpHeaders});
   }
 
   public debit(model: DebitModel) :Observable<DebitModel>{
-    return this.http.post<DebitModel>(this.host + '/debit', model);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.post<DebitModel>(this.host + '/debit', model, {headers:httpHeaders});
   }
 
   public getById(id: string) :Observable<OperationModel>{
-    return this.http.get<OperationModel>(this.host + '/get/' + id);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.get<OperationModel>(this.host + '/get/' + id, {headers:httpHeaders});
   }
 
   public getHistory(accountId: string, page: number, size: number): Observable<HistoryModel>{
-    return this.http.get<HistoryModel>(this.host + '/' + accountId + '/all?page=' + page + '&size=' +size);
+    let httpHeaders: HttpHeaders = this.getHeader();
+    return this.http.get<HistoryModel>(this.host + '/' + accountId + '/all?page=' + page + '&size=' +size, {headers:httpHeaders});
   }
 }
