21c42c1f4de519cec427d3471c6c7ef5aa7d678a
BrodyGaudel <brodymounanga@gmail.com>
2024-05-30 02:53:31 +0100

    Update gateway service to v3.2.6

--- gateway-service/src/test/java/org/mounanga/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/org/mounanga/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.brodygaudel.gatewayservice;
+package org.mounanga.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
